8c8eb368e973600dd4277835c9cd704ba2fbce32
hendrixjoseph <hendixjoseph@aol.com>
2024-04-15 13:49:37 -0400

    update dependency versions

--- src/test/java/com/joehxblog/spring/ContentSecurityPolicyBuilderTest.java
+++ src/test/java/com/joehxblog/spring/ContentSecurityPolicyBuilderTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import com.joehxblog.spring.csp.ContentSecurityPolicy;
-import com.joehxblog.spring.csp.ContentSecurityPolicyBuilder;
 import com.joehxblog.spring.csp.directive.FetchDirective;
 import com.joehxblog.spring.csp.directive.Directive;
 import com.joehxblog.spring.csp.value.KeywordValue;
